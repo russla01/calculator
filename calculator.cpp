4656b9a479064f2376f606b790efa62299076db6
Ryan Bennett <bennry01@luther.edu>
2013-12-10 17:34:15 -0600

    displays are working, but the numbers aren't correct yet

--- calculator.cpp
+++ calculator.cpp
@@ -1,18 +1,21 @@
-//calc
-
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Box.H>
 #include <FL/Fl_Button.H>
 #include <FL/fl_ask.H>
 #include <vector>
-#include "numButton.h"
+#include <string>
+#include <iostream>
 #include <FL/Fl_Output.H>
 
 using namespace std;
 
 vector <int> elements;
 
+Fl_Output *out4;
+Fl_Output *out3;
+Fl_Output *out2;
+Fl_Output *out1;
 
 void push(int value){
   elements.push_back(value);
@@ -49,95 +52,103 @@ int fourth(){
     return 0;
 }
 //end display functions
-void updateDisplay(){
-  out4->value((to_string(fourth())).c_str());
+
+void updateDisplays(){
+  out4->value( (to_string(fourth())).c_str() );
+  out3->value( (to_string(third())).c_str() );
+  out2->value( (to_string(second())).c_str() );
+  out1->value( (to_string(first())).c_str() );
 }
+
 void numButton(Fl_Widget *w, void *) {
-  NumberButton *myButton = (NumberButton *)w;
-  int num = myButton->getNum();
-  myButton->copy_label(std::to_string(num).c_str());
+  push(1);
+  updateDisplays();
 }
 
 void opAddition(Fl_Widget *w,void *){
-  Fl_Button *myButton = (Fl_Button *)w;
+  cout << "+" << endl;
 }
 
 void opSubtraction(Fl_Widget *w,void *){
-  Fl_Button *myButton = (Fl_Button *)w;
+  cout << "-" << endl;
 }
 
 void opMultiplication(Fl_Widget *w,void *){
-  Fl_Button *myButton = (Fl_Button *)w;
+   cout << "*" << endl;
 }
 
 void opDivision(Fl_Widget *w,void *){
-  Fl_Button *myButton = (Fl_Button *)w;
+   cout << "/" << endl;
 }
 void opExpo(Fl_Widget *w,void *){
-  Fl_Button *myButton = (Fl_Button *)w;
+   cout << "^" << endl;
 }
 
 void opSqrt(Fl_Widget *w,void *){
-  Fl_Button *myButton = (Fl_Button *)w;
+   cout << "sqrt" << endl;
 }
 void opSign(Fl_Widget *w,void *){
-  Fl_Button *myButton = (Fl_Button *)w;
+   cout << "sign" << endl;
 }
 void opEnter(Fl_Widget *w,void *){
-  Fl_Button *myButton = (Fl_Button *)w;
+   cout << "enter" << endl;
 }
 void opDrop(Fl_Widget *w,void *){
-  Fl_Button *myButton = (Fl_Button *)w;
+   cout << "drop" << endl;
 }
+
 
 int main(int argc, char *argv[])
 {
+  
   //create window
   Fl_Window *window = new Fl_Window(400,600);
 
   //displays
-  Fl_Output *out4 = new Fl_Output(5,5,400,25,"0");
+  
+  out4 = new Fl_Output(5,5,400,25,"0");
   out4->box(FL_UP_BOX);
   out4->textsize(12);
-  Fl_Output *out3 = new Fl_Output(5,35,400,25,"0");
+  out3 = new Fl_Output(5,35,400,25,"0");
   out3->box(FL_UP_BOX);
   out3->textsize(12);
-  Fl_Output *out2 = new Fl_Output(5,65,400,25,"0");
+  out2 = new Fl_Output(5,65,400,25,"0");
   out2->box(FL_UP_BOX);
   out2->textsize(12);
-  Fl_Output *out1 = new Fl_Output(5,95,400,25,"0");
+  out1 = new Fl_Output(5,95,400,25,"0");
   out1->box(FL_UP_BOX);
   out1->textsize(12);
+  
 
-  //added button
- NumberButton *b1 = new NumberButton(20, 200, 80,80,"&Beep",1);
+ //added button
+ Fl_Button *b1 = new Fl_Button(20, 200, 80,80,"1");
  b1->callback(numButton,0);
 
- NumberButton *b2 = new NumberButton(105, 200, 80,80,"&Beep",2);
+ Fl_Button *b2 = new Fl_Button(105, 200, 80,80,"2");
  b2->callback(numButton,0);
 
- NumberButton *b3 = new NumberButton(190, 200, 80,80,"&Beep",3);
+ Fl_Button *b3 = new Fl_Button(190, 200, 80,80,"3");
  b3->callback(numButton,0);
 
- NumberButton *b4 = new NumberButton(20, 290, 80,80,"&Beep",4);
+ Fl_Button *b4 = new Fl_Button(20, 290, 80,80,"4");
  b4->callback(numButton,0);
 
- NumberButton *b5 = new NumberButton(105, 290, 80,80,"&Beep",5);
+ Fl_Button *b5 = new Fl_Button(105, 290, 80,80,"5");
  b5->callback(numButton,0);
 
- NumberButton *b6 = new NumberButton(190, 290, 80,80,"&Beep",6);
+ Fl_Button *b6 = new Fl_Button(190, 290, 80,80,"6");
  b6->callback(numButton,0);
 
- NumberButton *b7 = new NumberButton(20, 380, 80,80,"&Beep",7);
+ Fl_Button *b7 = new Fl_Button(20, 380, 80,80,"7");
  b7->callback(numButton,0);
 
- NumberButton *b8 = new NumberButton(105, 380, 80,80,"&Beep",8);
+ Fl_Button *b8 = new Fl_Button(105, 380, 80,80,"8");
  b8->callback(numButton,0);
 
- NumberButton *b9 = new NumberButton(190, 380, 80,80,"&Beep",9);
+ Fl_Button *b9 = new Fl_Button(190, 380, 80,80,"9");
  b9->callback(numButton,0);
     
- NumberButton *b0 = new NumberButton(105, 470, 80,80,"&Beep",0);
+ Fl_Button *b0 = new Fl_Button(105, 470, 80,80,"0");
  b0->callback(numButton,0);
 
  Fl_Button *addition = new Fl_Button(300,200, 50, 50,"+");
@@ -161,17 +172,15 @@ int main(int argc, char *argv[])
  Fl_Button *sign = new Fl_Button(300,530, 50, 50,"+/-");
  sign->callback (opSign,0);
 
-
  Fl_Button *enter = new Fl_Button(200,485, 50, 50,"ENTER");
  enter->callback (opEnter,0);
  
  Fl_Button *drop = new Fl_Button(35,485, 50, 50,"DROP");
  drop->callback (opDrop,0);
- 
+  
 
-//cout << "hello" << endl;
-// cout << "good bye" << endl;
   window->end();
   window->show(argc,argv);
+
   return Fl::run();
 }
